441d67ddb9818b5fdd41b2c554cf7997be781e05
Madara228 <karagishiev14@mail.ru>
2018-07-10 12:23:40 +0300

    new VR

---
+++ Assets/MovingScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingScript : MonoBehaviour {
+
+    public Transform camera;
+
+    public float toggleAngle = 30f;
+
+    public float speed = 3f;
+
+    public bool moveForward;
+
+    private CharacterController cc;
+
+
+	void Start () {
+        cc = GetComponent<CharacterController>();
+	}
+	
+	
+	void Update () {
+        if (camera.eulerAngles.x > toggleAngle && camera.eulerAngles.x < 90f)
+        {
+            moveForward = true;
+        }
+        else
+        {
+            moveForward = false;
+        }
+        if (moveForward)
+        {
+            Vector3 forward = camera.TransformDirection(Vector3.forward);
+
+            cc.SimpleMove(forward * speed);
+        }
+	}
+}
---
+++ Assets/trigeerred.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trigeerred : MonoBehaviour {
+
+	public void triggered()
+    {
+        transform.Rotate(new Vector3(12f,5f,5f));
+        print("it's fucking work!"); 
+    }
+}
